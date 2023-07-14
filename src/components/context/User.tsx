902a9bcad9b59de61bd3cfb1e6ff63bf2abaea6f
Felix Owolabi <owolabifelix78@gmail.com>
2023-07-14 15:49:09 +0100

    react-typeScript

---
+++ src/components/context/User.tsx
@@ -0,0 +1,13 @@
+export const User = ()=>{
+    const handleLogIn = () => {}
+    const handleLogOut = () => {}
+    return (
+        <div>
+            <button onClick={handleLogIn}>LogIn</button>
+            <button onClick={handleLogOut}>LogOut</button>
+            <div>User name is </div>
+            <div>User email is </div>
+        </div>
+    )
+
+}
